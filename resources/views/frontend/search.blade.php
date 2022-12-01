bb67ed13ae44d97ddcec4a4fe66bf09cc6af0bb3
AlbertPotter <smilepotter0128@gmail.com>
2022-12-01 11:21:57 -0800

    yes

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.frontend-main ')
 
 @section('content')
+@if(user()->subscription()->plan->isFree())
 <div class="card-header bg-100">
-    <b style="    font-size: 24px;"> {{ $results_count }} </b>results found for "<b>{{request()->query('q')}}</b>"
+    <b style="font-size: 24px;"> {{ $results_count }} </b>results found for "<b>{{request()->query('q')}}</b>"
 </div>
+@endif
 <div class="homepage-section m-auto align-items-center text-center d-flex flex-column justify-content-center homepage-section-hero @if(request()->query('q')) d-none @endif" style="background:linear-gradient(180deg, rgba(18, 18, 18, 0) -26%, rgba(18, 18, 18, 0.787848) -4.22%, #121212 58%), url(http://localhost:8000/images/bg/hero1.jpg);">
     <div style="height:70px" class="mobile-d"></div>
     <p class="row col-md-6 col-sm-12 container h1 text-center h-auto">Get on the right playlist & reach your future fans</p>
